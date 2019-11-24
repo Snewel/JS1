f34bbb73112ba4f8bc57144fa7c563b6a38de5f5
Snewel <wot7263@mail.ru>
2019-11-24 13:39:36 +0300

    Сделал ДЗ №1

---
+++ js/main.js
@@ -0,0 +1,48 @@
+
+// Всё выводил в консоль, ибо в алерт вырвиглазно
+
+let Tc = 12
+perevod(Tc)
+function perevod () { // Первое задание
+    let Tf = (9 / 5) * Tc + 32
+    console.log(Tf)
+}
+
+let admin
+let name = 'Василий'
+kek(name)
+function kek () { // Второе задание
+    admin = name
+    console.log(admin)
+} 
+
+let a = 20
+let b = 30
+change(a,b)
+function change() { // Третье задание 
+    b += a  
+    a = b - a
+    b -= a
+    console.log(a,b)
+} 
+
+let test = 100 + '108' // Четвёртое задание
+console.log(test)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
