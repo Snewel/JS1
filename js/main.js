8c4bf4f8aef8a324342d4365e7db31628ba67f08
Snewel <wot7263@mail.ru>
2020-02-20 15:32:43 +0300

    Проект сайта "Автосервис"

---
+++ js/main.js
@@ -0,0 +1,15 @@
+$(function () {
+
+
+})
+
+function mobile() {   
+    let phone = $('.menu')
+    if ( phone.css('display') == 'none') {
+        phone.show()
+    }
+    else {
+        phone.hide()
+    }
+}
+
